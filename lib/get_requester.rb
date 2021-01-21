bbf605efe15f588653ad0705f7c9f372e4770e53
YOUR_GITHUB_USER_NAME <YOUR_GITHUB_EMAIL_ADDRESS>
2021-01-21 18:42:37 -0500

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+require 'pry'
+
+class GetRequester
+
+    attr_accessor :url 
+
+        def initialize(url)
+            @url = url
+        end
+
+        def get_response_body
+            
+            # binding.pry
+        end
+
+
+end
+
+URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+get_requester = GetRequester.new(URL)
+p get_requester.get_response_body
